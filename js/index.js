d1b67de8bb46dba8cb6e8dbe219424ebc2a97e0e
samandareo <smdeveloper04@gmail.com>
2023-02-11 14:00:11 +0500

    upg

--- js/index.js
+++ js/index.js
@@ -5,4 +5,22 @@ function changeStyleOfham() {
         } else {
           element.style.display = "flex";
         }
-    }
+}
+
+function send(params) {
+  var templateParams = {
+      main_topic : document.getElementById("topic").value,
+      from_name : document.getElementById("name").value,
+      message : document.getElementById("message-text").value,
+      email : document.getElementById("email").value,
+      reply_to : document.getElementById("email").value
+  };
+   
+  emailjs.send("service_rlz2pcb", "template_zpb3s7o" , templateParams)
+      .then(function(response) {
+         console.log('SUCCESS!', response.status);
+      }, function(error) {
+         console.log('FAILED...', error);
+      });
+  
+}
